2f2cb04f7e5c5ff6ca9d830d9498aa929ee29f66
Kevin Albrecht <onlyafly@gmail.com>
2016-02-20 12:02:47 +0100

    Update for Melissa needs.

--- splode.js
+++ splode.js
@@ -1,10 +1,7 @@
-function main() {
-  var sourceElement = document.getElementById('splodeData');
-  var data = jsyaml.load(sourceElement.innerText);
-  var contentElement = document.getElementById('splodeContent');
-  var n = data.length;
-  var i = Math.floor((Math.random() * n)); // generate a random number from 0 to length-1
-  contentElement.innerText = data[i];
+function splode(sourceId, destinationId) {
+  var sourceElement = document.getElementById(sourceId);
+  var destinationElement = document.getElementById(destinationId);
+  var data = sourceElement.children;
+  var i = Math.floor((Math.random() * data.length)); // generate a random number from 0 to length-1
+  destinationElement.innerHTML = data[i].innerHTML;
 }
-
-main();
